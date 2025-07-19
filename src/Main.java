ae18efb904151d5e062d2571363bfc8555f87f2d
varun7568 <19tandulwadikar_v@bexleygs.co.uk>
2025-07-19 19:03:30 +0100

    reports screen works with hardcoded data

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         System.out.println("SEQUENCE: main started");
-        HomeScreen gui = new HomeScreen();
         System.out.println("SEQUENCE: main finished");
-
-        }
+        HomeScreen homescreen = new HomeScreen();
     }
 
-    //prevent duplicate entries
+}
